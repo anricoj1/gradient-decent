a5146b057c6dbfe3a36235569626f2e798d25449
jason <anricoj1@southernct.edu>
2019-04-22 00:16:11 -0400

    fixed stochastic file + modified readme

--- stoch.py
+++ stoch.py
@@ -5,86 +5,46 @@ import pylab
 from scipy import stats
 import math
 
-def stochastic(alpha, x, y, ep, max_iter=10000000):
-    converged = False
-    m = x.shape[0] # number of samples
-    iter = 0
 
-    x_transpose = x.transpose()
+# make y pred function
+def predict(row, w):
+    yhat = w[0]
+    for i in range(len(row)-1):
+        yhat += w[i + 1] * row[i]
 
-    w0 = 0.25
-    w1 = 0.3
+    return yhat  #returns yhat (estimated y)
 
-    while not converged:
-        wx = get_wx(x, y, m, w0, w1)
-        hwx = get_hwx(wx, m)
+# stochastic gradient descent func uses (x, alpha, iterations)
+def sgd(x, alpha, iterations):
+    weight = [0.0 for i in range(len(x[0]))] #set weight
+    for iter in range(iterations):
+        sum_err = 0
+        for row in x: #loop through x array
+            yhat = predict(row, weight)
+            err = yhat - row[-1]  # estimate error
+            sum_err += err**2
+            weight[0] = weight[0] - alpha * err
+            for i in range(len(row)-1):
+                weight[i + 1] = weight[i + 1] - alpha * err * row[i]
+        print('Iteration: %d | Predicted(y) = %.5f | Sum Error = %.5f' %(iter, yhat, sum_err))
 
-    return get_grad(x, y, w0, w1, m, hwx, iter)
-
-def get_wx(x, y, m, w0, w1):
-    m = x.shape[0]
-    for i in range(m):
-        wx = (w0 + w1*x[i] - y)**2
-
-    return wx
-
-
-def get_hwx(wx, m):
-    m = x.shape[0]
-    for i in range(m):
-        hwx = 1/(1 + math.exp(-wx[i]))
-
-    return hwx
-
-def get_grad(x, y, w0, w1, m, hwx, iter):
-    m = x.shape[0]
-    cost = sum([(w0 + w1*x[i] - y[i]) for i in range(m)])
-    for i in range(m):
-        g0 = w0 + 0.0001 * (y - hwx[i])
-        g1 = w1 + 0.0001 * (y - hwx[i]) * x[i]
-
-        tmp0 = w0 + alpha * g0
-        tmp1 = w1 + alpha * g1
-
-        w0 = tmp0
-        w1 = tmp1
-
-        err = sum( [ (w0 + w1*x[i] - y[i])**2 for i in range(m)] )
-
-        c = abs(cost - err)
-
-        if c <= 0.0000000001:
-            print('Converged after %s Iterations! With Convergence = %.10f' %(iter, c))
-            converged = True
-
-        J = err
-        iter += 1
-
-        if iter == 100000000:
-            print('Max Iter')
-            converged = True
+    return weight
 
 
-    return w0, w1
 
 if __name__ == '__main__':
+    x = np.array([[2,5], [4,7], [6,14], [7,14], [8,17], [10, 19]]) #declare x array
+    alpha = 0.0001  #learning rate
+    iterations = 200000 #iter count
+    w = [0.25, 0.30] #inital weights
+    
+    new_weights = sgd(x, alpha, iterations)
+    print('New Weights: w0 = %.5f w1 = %.5f' %(new_weights[0], new_weights[1]))  #new weights
 
-    x = np.array([2,4,6,7,8,10])
-
-    x, y = make_regression(n_samples=100, n_features=1, n_informative=1,
-                        random_state=0, noise=35)
-
-    print('x.shape = %s y.shape = %s' %(x.shape, y.shape))
-
-    alpha = 0.0001
-    ep = 0.0000000001
-
-    theta0, theta1 = stochastic(alpha, x, y, ep, max_iter=1000000)
-    print('w0 = %.10f w1 = %.10f' %(theta0, theta1))
-
-    slope, intercept, r_value, p_value, slope_std_error = stats.linregress(x[:,0], y)
-
+    x, y = make_regression(n_samples=6, n_features=1, n_informative=1, random_state=0, noise=35)
+    slope, intercept, r_value, p_value, slope_std_error = stats.linregress(x[:,0], y) #use stats
     print('Intercept = %s slope = %s' %(intercept, slope))
+
     x5 = intercept * 5 + slope
     print('When x = 5, y = %s' %(x5))
     neg100 = intercept * -100 + slope
@@ -92,8 +52,9 @@ if __name__ == '__main__':
     x100 = intercept * 100 + slope
     print('When x = 100, y = %s' %(x100))
 
+    #plot
     for i in range(x.shape[0]):
-        y_pred = theta0 + theta1*x
+        y_pred = new_weights[0] + new_weights[1]*x
 
     pylab.plot(x,y,'o')
     pylab.plot(x,y_pred,'k-')
