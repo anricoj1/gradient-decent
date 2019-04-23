3f372a45f4b399d6eae83c09bea208cb087bddb8
jason <anricoj1@southernct.edu>
2019-04-23 00:46:49 -0400

    submitted hw

--- batch.py
+++ batch.py
@@ -8,39 +8,37 @@ from scipy import stats
 import math
 
 
-def batch_gradient_descent(alpha, x, y, ep=0.0000000001, max_iter=1000000):
+def batch_gradient_descent(alpha, x, y, ep=0.0000000001, max=1000000):
     converged = False # for while loop
     iter = 0 # starting iteration
     m = x.shape[0] # number of samples for range
 
 
     #inital weights
-    w0 = 0.25
-    w1 = 0.3
+    w = [0.25, 0.25]
 
-
-    J = sum([(w0 + w1*x[i] - y[i])**2 for i in range(m)])
+    # total error
+    J = sum([(w[0] + w[1]*x[i] - y[i])**2 for i in range(m)])
 
     # Iterate until we see convergence or "ep"
     while not converged:
         # for each sample we find the gradient 1/m * w0 + w1(xi) - y(i)
-
-        g0 = 1.0/m * sum([(w0 + w1*x[i] - y[i]) for i in range(m)])
-        g1 = 1.0/m * sum([(w0 + w1*x[i] - y[i])*x[i] for i in range(m)])
+        g0 = 1.0/m * sum([(w[0] + w[1]*x[i] - y[i]) for i in range(m)])
+        g1 = 1.0/m * sum([(w[0] + w[1]*x[i] - y[i])*x[i] for i in range(m)])
 
         # update weights
-        tmp0 = w0 - alpha * g0
-        tmp1 = w1 - alpha * g1
+        tmp0 = w[0] - alpha * g0
+        tmp1 = w[1] - alpha * g1
 
-        w0 = tmp0
-        w1 = tmp1
+        w[0] = tmp0
+        w[1] = tmp1
 
-        # sum of the error
-        err = sum( [ (w0 + w1*x[i] - y[i])**2 for i in range(m)] )
+        # mean squared error
+        err = sum([(y[i] - (w[1]*x[i] + w[0]))**2 for i in range(m)])
 
         c = abs(J-err)
 
-        print('Iteration = %s | EP = %.10f' %(iter, c))
+        print('Iteration = %s | error = %s | EP = %.10f' %(iter, err, c))
 
         if c <= ep:
             print('Converged after %s Iterations! With Convergence = %.10f' %(iter, c))
@@ -50,28 +48,24 @@ def batch_gradient_descent(alpha, x, y, ep=0.0000000001, max_iter=1000000):
         iter += 1 #increase iteration
 
         # if we reach max iterations of 1,000,000 converged is True
-        if iter == max_iter:
+        if iter == max:
             print('Max Iterations Reached!')
             converged = True
 
 
-    return w0, w1
+    return w[0], w[1]
 
 if __name__ == '__main__':
 
+    alpha = 0.0001 #learning rate
+    ep = 0.0000000001
     x = np.array([2,4,6,7,8,10])
     y = np.array([5,7,14,14,17,19])
 
+    w0, w1 = batch_gradient_descent(alpha, x, y, ep, max=1000000) #call batch to determine w0,w1
+    print('w0 = %.10f w1 = %.10f' %(w0, w1))
+
     x, y = make_regression(n_samples=6, n_features=1, n_informative=1, random_state=0, noise=35)
-
-
-    print('x.shape = %s y.shape = %s' %(x.shape, y.shape))
-
-    alpha = 0.0001
-    ep = 0.0000000001
-
-    theta0, theta1 = batch_gradient_descent(alpha, x, y, ep, max_iter=1000000)
-    print('w0 = %.10f w1 = %.10f' %(theta0, theta1))
 
     slope, intercept, r_value, p_value, slope_std_error = stats.linregress(x[:,0], y)
     print('Intercept = %s slope = %s' %(intercept, slope))
@@ -83,11 +77,9 @@ if __name__ == '__main__':
     x100 = intercept * 100 + slope
     print('When x = 100, y = %s' %(x100))
 
-
     for i in range(x.shape[0]):
-        y_pred = theta0 + theta1*x
+        y_pred = w0 + w1*x
 
     pylab.plot(x,y,'o')
     pylab.plot(x,y_pred,'k-')
     pylab.show()
-    print('Done!')
--- stoch.py
+++ stoch.py
@@ -4,29 +4,33 @@ from sklearn.datasets.samples_generator import make_regression
 import pylab
 from scipy import stats
 import math
+import pdb
 
 
 # make y pred function
 def predict(row, w):
     yhat = w[0]
+    m = 6
     for i in range(len(row)-1):
         yhat += w[i + 1] * row[i]
 
     return yhat  #returns yhat (estimated y)
 
 # stochastic gradient descent func uses (x, alpha, iterations)
-def sgd(x, alpha, iterations):
+def sgd(x, alpha, iterations, ep=0.0000000001):
     weight = [0.0 for i in range(len(x[0]))] #set weight
     for iter in range(iterations):
-        sum_err = 0
-        for row in x: #loop through x array
+        y = np.random.randint(20, size=6) #set random y np array
+        J = 0
+        for row in x:
             yhat = predict(row, weight)
             err = yhat - row[-1]  # estimate error
-            sum_err += err**2
+            J = err**2
+            c = abs(J-err)
             weight[0] = weight[0] - alpha * err
             for i in range(len(row)-1):
                 weight[i + 1] = weight[i + 1] - alpha * err * row[i]
-        print('Iteration: %d | Predicted(y) = %.5f | Sum Error = %.5f' %(iter, yhat, sum_err))
+        print('Iter: %d | yHat = %.3f | Error = %.5f | Differ = %.10f ' %(iter, yhat, J, c))
 
     return weight
 
@@ -35,10 +39,11 @@ def sgd(x, alpha, iterations):
 if __name__ == '__main__':
     x = np.array([[2,5], [4,7], [6,14], [7,14], [8,17], [10, 19]]) #declare x array
     alpha = 0.0001  #learning rate
+    ep = 0.0000000001 #convergence
     iterations = 200000 #iter count
-    w = [0.25, 0.30] #inital weights
-    
-    new_weights = sgd(x, alpha, iterations)
+    w = [0.25, 0.25] #inital weights
+
+    new_weights = sgd(x, alpha, iterations, ep)
     print('New Weights: w0 = %.5f w1 = %.5f' %(new_weights[0], new_weights[1]))  #new weights
 
     x, y = make_regression(n_samples=6, n_features=1, n_informative=1, random_state=0, noise=35)
@@ -59,4 +64,3 @@ if __name__ == '__main__':
     pylab.plot(x,y,'o')
     pylab.plot(x,y_pred,'k-')
     pylab.show()
-    print('Done!')
